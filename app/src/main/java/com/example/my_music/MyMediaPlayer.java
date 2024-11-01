6a96deacf5efba23d4587ba4e9e9a5237ce42426
Sarmaj <156239870+Sarmaj@users.noreply.github.com>
2024-11-01 15:42:10 -0500

    encapsulated current index

--- app/src/main/java/com/example/my_music/MyMediaPlayer.java
+++ app/src/main/java/com/example/my_music/MyMediaPlayer.java
@@ -1,16 +1,27 @@
 package com.example.my_music;
 
 import android.media.MediaPlayer;
- 
+
 public class MyMediaPlayer {
-    static MediaPlayer instance;
- 
-    public static MediaPlayer getInstance(){
-        if(instance == null){
+    private static MediaPlayer instance;
+    private static int currentIndex = -1;
+
+    private MyMediaPlayer() {
+        
+    }
+
+    public static MediaPlayer getInstance() {
+        if (instance == null) {
             instance = new MediaPlayer();
         }
         return instance;
     }
- 
-    public static int currentIndex = -1;
+
+    public static int getCurrentIndex() {
+        return currentIndex;
+    }
+
+    public static void setCurrentIndex(int index) {
+        currentIndex = index;
+    }
 }
